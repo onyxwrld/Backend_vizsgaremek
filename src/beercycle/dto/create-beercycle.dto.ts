b88b6223901f0996768a5f511cb8f357b7980bdf
Csontos <csontostibor48@gmail.com>
2024-02-09 18:01:56 +0100

    dtoclass

--- src/beercycle/dto/create-beercycle.dto.ts
+++ src/beercycle/dto/create-beercycle.dto.ts
@@ -1,1 +1,22 @@
-export class CreateBeercycleDto {}
+
+export class CreateUserDto{
+
+}
+export class CreateBicycleDto{
+
+}
+export class CreateMenuDto{
+
+}
+export class CreateOrderDto{
+
+}
+export class CreateReservationDto{
+
+}
+export class CreateTorzsAdatokDto{
+
+}
+export class CreateWorkerDto{
+
+}
