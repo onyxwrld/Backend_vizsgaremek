728b61cbd651f08928b87964a7b57f30918ab249
Csontos <csontostibor48@gmail.com>
2024-02-13 11:55:50 +0100

    no

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { WorkerModule } from './worker/worker.module';
 import { OrderModule } from './order/order.module';
 import { TorzsAdatokModule } from './torzs-adatok/torzs-adatok.module';
 import { BicycleModule } from './bicycle/bicycle.module';
-
+import { ReservationModule } from './reservation/reservation.module';
 @Module({
   imports: [BeercycleModule, UserModule, WorkerModule, OrderModule, TorzsAdatokModule, BicycleModule],
   controllers: [AppController],
