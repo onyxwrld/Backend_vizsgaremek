c0cf74629eaf5e246083cb2b8db42d68b8b009ab
Szabó <szabo.beci10@gmail.com>
2024-02-13 11:58:12 +0100

    menu

---
+++ src/menu/dto/create-menu.dto.ts
@@ -0,0 +1,13 @@
+export class CreateMenuDto {
+    name:string;
+    type:string;
+    price:string;
+}
+/*
+*model Menu {
+  id    Int    @id @default(autoincrement())
+  name  String
+  type  String
+  price Int
+}
+*/
--- src/torzs-adatok/dto/create-torzs-adatok.dto.ts
+++ src/torzs-adatok/dto/create-torzs-adatok.dto.ts
@@ -1,18 +1,20 @@
-import { IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, IsNumber, IsPhoneNumber } from "class-validator";
 
 export class CreateTorzsAdatokDto {
-    @IsNotEmpty({message:'A név megadása kötelező'})
+    @IsNotEmpty({message:'A telefon megadása kötelező'})
+    @IsNumber()
+    @IsPhoneNumber('HU', { message: 'Helytelen telefonszám' })
     phone_number: number;
-    email:string;
-    opening_hours : string;
-    location: string;
-    company_id: number;
 
+    @IsNotEmpty({message:'Az email megadása kötelező'})
+    @IsEmail({}, { message: 'Helytelen email formátum' })
+    email:string;
+
+    @IsNotEmpty({message:'Nyitási adatok megadása kötelező'})
+    opening_hours : string;
+
+    @IsNotEmpty({message:'Hely megadása megadása kötelező'})
+    location: string;
+
+    company_id: number;
 }
-/*
-*  phone_number  Int    @id @default(autoincrement())
-  email         String @unique
-  opening_hours String
-  location      String
-  company_id    Int    @unique
-*/
