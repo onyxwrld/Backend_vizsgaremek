4a0704404eba1380271feefa3b54151c87a974d6
Szabó <szabo.beci10@gmail.com>
2024-02-13 11:59:18 +0100

    meun

--- src/menu/dto/create-menu.dto.ts
+++ src/menu/dto/create-menu.dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty, IsNumber } from "class-validator";
+
 export class CreateMenuDto {
+    @IsNotEmpty({message:'A név megadása kötelező'})
     name:string;
+    @IsNotEmpty({message:'A típus megadása kötelező'})
     type:string;
+    @IsNumber()
     price:string;
 }
 /*
