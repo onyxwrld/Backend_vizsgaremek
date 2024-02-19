e3d622f4f1d50a364fc3cfe3587d5dfefb492acd
Szabó <szabo.beci10@gmail.com>
2024-02-19 13:19:55 +0100

    kapcsolatok

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
-import { BeercycleModule } from './beercycle/beercycle.module';
 import { UserModule } from './user/user.module';
 import { WorkerModule } from './worker/worker.module';
 import { OrderModule } from './order/order.module';
 import { TorzsAdatokModule } from './torzs-adatok/torzs-adatok.module';
 import { BicycleModule } from './bicycle/bicycle.module';
 import { ReservationModule } from './reservation/reservation.module';
-import { ReservationModule } from './reservation/reservation.module';
 @Module({
-  imports: [BeercycleModule, UserModule, WorkerModule, OrderModule, TorzsAdatokModule, BicycleModule, ReservationModule],
+  imports: [BicycleModule, UserModule, WorkerModule, OrderModule, TorzsAdatokModule, BicycleModule, ReservationModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
