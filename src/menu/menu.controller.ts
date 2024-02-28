9b68edf671ce4cc0c91a3fb1fc96a338f8ec536b
Petrik Diak <diak@petrik.hu>
2024-02-28 12:32:49 +0100

    Menu admin eleres

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,Request, BadRequestException, UseGuards, ForbiddenException } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { CreateMenuDto } from './dto/create-menu.dto';
 import { UpdateMenuDto } from './dto/update-menu.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 
 @Controller('menu')
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
 
   @Post()
-  create(@Body() createMenuDto: CreateMenuDto) {
-    return this.menuService.create(createMenuDto);
+  @UseGuards(AuthGuard('bearer'))
+  create(@Body() createMenuDto: CreateMenuDto,@Request() req) {
+    const user: User = req.user;
+    if(user.role != 'Admin'){
+      throw new ForbiddenException();
+    }else{
+    return this.menuService.create(createMenuDto);}
   }
 
   @Get()
@@ -26,8 +33,14 @@ export class MenuController {
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateMenuDto: UpdateMenuDto) {
-    try{ return await this.menuService.update(+id, updateMenuDto);
+  @UseGuards(AuthGuard('bearer'))
+  async update(@Param('id') id: string, @Body() updateMenuDto: UpdateMenuDto,@Request() req) {
+    const user: User = req.user;
+    try{if(user.role != 'Admin'){
+      throw new ForbiddenException();
+    }else{ 
+      return await this.menuService.update(+id, updateMenuDto);
+    }
     }catch{
       throw new BadRequestException('A keresett ID nem található')
     }
