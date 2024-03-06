0833ab8074ccf8e4a38ef5c05cab1467c486fa66
ricsi <ricsiszupkai.1@gmail.com>
2024-03-06 14:33:13 +0100

    tokenInfo hibajavítás

--- src/auth/token.strategy.ts
+++ src/auth/token.strategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from "@nestjs/passport";
 import {Strategy} from 'passport-http-bearer';
 import { AuthService } from "./auth.service";
 import { BadRequestException, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Token } from "@prisma/client";
 
 @Injectable()
 export class TokenStrategy extends PassportStrategy(Strategy){
@@ -12,8 +13,13 @@ export class TokenStrategy extends PassportStrategy(Strategy){
     }
     async validate(token:string){
         const user = this.authService.findUserByToken(token);
-        const tokenInfo = this.authService.findUserTokenInfo(token);
-        const expiresAt = (await tokenInfo).expiresAt 
+        let tokenInfo: Token;
+        try{
+            tokenInfo = await this.authService.findUserTokenInfo(token);
+        }
+        catch{throw new UnauthorizedException()}
+        
+        const expiresAt = tokenInfo.expiresAt 
         const nowDate = new Date()
         if(user == null){
             throw new UnauthorizedException();
