c0d4a2284e203359b7b28a8e2c4a5f57d4f57bcf
Szabó <szabo.beci10@gmail.com>
2024-03-22 12:30:37 +0100

    reservation

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -5,13 +5,29 @@ import { PrismaService } from 'src/prisma.service';
 @Injectable()
 export class ReservationService {
   constructor(private readonly db: PrismaService) { }
-  create(createReservationDto: CreateReservationDto, user_id: number) {
-    return this.db.reservation.create({
+  async create(createReservationDto: CreateReservationDto, user_id: number) {
+    const reservations = await this.db.reservation.create({
       include: {
         user: {
-          select: { basket: true } 
+          include:
+          {
+            basket:
+            {
+              include:
+              {
+                menu:
+                {
+                  select:
+                  {
+                    price:true
+                  }
+                }
+              }
+            }
+          }
         }
-      },
+      }
+    ,
       data: {
         state: "Pending",
         start_time: createReservationDto.start_time,
@@ -23,8 +39,20 @@ export class ReservationService {
             id:user_id
           }
         },
-      }
+      },
     });
+    for (let reservation of reservations){
+      let total_sum = 0;
+        for(let users of reservation.user.basket){
+          
+          for(let menuitems of users.menu ){
+            total_sum += menuitems.price
+          }
+          
+        }
+        ;reservation.total_amount = total_sum;
+     } 
+     return reservations;
   }
 
   async findAll(user_id:number) {
